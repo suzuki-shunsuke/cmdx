f1235e32e931a74055e2ae1a02c97a8fe0353be1
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2019-08-21 21:57:52 +0900

    build: fix fmt.sh

--- scripts/fmt.sh
+++ scripts/fmt.sh
@@ -1,5 +1,4 @@
-find . -type d -name node_modules -prune -o \
+find . \
   -type d -name .git -prune -o \
-  -type d -name vendor -prune -o \
   -type f -name "*.go" -print \
   | xargs gofmt -l -s -w
