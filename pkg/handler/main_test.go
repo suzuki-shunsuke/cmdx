a743b10a4062e7777af2ec49b1320daca29e28c4
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2019-08-22 09:04:49 +0900

    test: test newFlag

--- pkg/handler/main_test.go
+++ pkg/handler/main_test.go
@@ -21,3 +21,62 @@ func Test_setupApp(t *testing.T) {
 		},
 	}, app.Authors)
 }
+
+func Test_newFlag(t *testing.T) {
+	data := []struct {
+		title string
+		flag  Flag
+		exp   cli.Flag
+	}{
+		{
+			title: "bool",
+			flag: Flag{
+				Name:  "foo",
+				Short: "f",
+				Usage: "usage",
+				Env:   "FOO",
+				Type:  "bool",
+			},
+			exp: cli.BoolFlag{
+				Name:   "foo, f",
+				Usage:  "usage",
+				EnvVar: "FOO",
+			},
+		},
+		{
+			title: "string",
+			flag: Flag{
+				Name:    "foo",
+				Usage:   "usage",
+				Default: "default value",
+				Env:     "FOO",
+			},
+			exp: cli.StringFlag{
+				Name:   "foo",
+				Usage:  "usage",
+				Value:  "default value",
+				EnvVar: "FOO",
+			},
+		},
+		{
+			title: "required",
+			flag: Flag{
+				Name:     "foo",
+				Usage:    "usage",
+				Env:      "FOO",
+				Required: true,
+			},
+			exp: cli.StringFlag{
+				Name:     "foo",
+				Usage:    "usage",
+				EnvVar:   "FOO",
+				Required: true,
+			},
+		},
+	}
+	for _, d := range data {
+		t.Run(d.title, func(t *testing.T) {
+			assert.Equal(t, d.exp, newFlag(d.flag))
+		})
+	}
+}
