582df5dc2c2f438194b0958436443ff82c292ecb
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2020-01-02 22:25:04 +0900

    build: rename functions

--- scripts/codecov-test.sh
+++ scripts/codecov-test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # https://github.com/codecov/example-go#caveat-multiple-files
 
-echoEval() {
+ee() {
   echo "+ $*"
   eval "$@"
 }
@@ -10,7 +10,7 @@ echo "" > coverage.txt
 
 for d in $(go list ./...); do
   echo "$d"
-  echoEval go test -race -coverprofile=profile.out -covermode=atomic "$d" || exit 1
+  ee go test -race -coverprofile=profile.out -covermode=atomic "$d" || exit 1
   if [ -f profile.out ]; then
     cat profile.out >> coverage.txt
     rm profile.out
--- scripts/githook.sh
+++ scripts/githook.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 
-echoEval() {
+ee() {
   echo "+ $*"
   eval "$@"
 }
 
 cd "$(dirname "$0")"/.. || exit 1
 if [ ! -f .git/hooks/pre-commit ]; then
-  echoEval ln -s ../../githook/pre-commit.sh .git/hooks/pre-commit || exit 1
+  ee ln -s ../../githook/pre-commit.sh .git/hooks/pre-commit || exit 1
 fi
-echoEval chmod a+x githook/*
+ee chmod a+x githook/*
