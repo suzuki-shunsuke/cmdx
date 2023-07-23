9e71b3600545f2ced75206c65f0ad547b2055be9
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2023-07-24 08:14:49 +0900

    refactor: replace suzuki-shunsuke/go-timeout to exec.CommandContext, Cmd.Cancel, and Cmd.WaitDelay
    go-timeout was deprecated.

--- pkg/execute/execute.go
+++ pkg/execute/execute.go
@@ -9,7 +9,6 @@ import (
 	"time"
 
 	"github.com/suzuki-shunsuke/go-error-with-exit-code/ecerror"
-	"github.com/suzuki-shunsuke/go-timeout/timeout"
 )
 
 type Executor struct{}
@@ -33,12 +32,23 @@ type Timeout struct {
 	KillAfter time.Duration
 }
 
+func setCancel(cmd *exec.Cmd, waitDelay time.Duration) *exec.Cmd {
+	cmd.Cancel = func() error {
+		return cmd.Process.Signal(os.Interrupt) //nolint:wrapcheck
+	}
+	if waitDelay == 0 {
+		waitDelay = 1000 * time.Hour
+	}
+	cmd.WaitDelay = waitDelay
+	return cmd
+}
+
 func (exc *Executor) Run(ctx context.Context, params *Params) error {
 	shell := params.Shell
 	if len(shell) == 0 {
 		shell = []string{"sh", "-c"}
 	}
-	cmd := exec.Command(shell[0], append(shell[1:], params.Script)...) //nolint:gosec
+	cmd := exec.CommandContext(ctx, shell[0], append(shell[1:], params.Script)...) //nolint:gosec
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Stdin = os.Stdin
@@ -51,10 +61,8 @@ func (exc *Executor) Run(ctx context.Context, params *Params) error {
 	if params.DryRun {
 		return nil
 	}
-	runner := timeout.NewRunner(params.Timeout.KillAfter)
-	runner.SetSigKillCaballback(func(targetID int) {
-		fmt.Fprintf(os.Stderr, "send SIGKILL to %d\n", targetID)
-	})
+
+	setCancel(cmd, params.Timeout.KillAfter)
 
 	if params.Timeout.Duration > 0 {
 		c, cancel := context.WithTimeout(ctx, params.Timeout.Duration)
@@ -68,7 +76,7 @@ func (exc *Executor) Run(ctx context.Context, params *Params) error {
 			fmt.Fprintf(os.Stderr, "command is terminated by timeout: %d seconds\n", params.Timeout.Duration)
 		}
 	}()
-	if err := runner.Run(ctx, cmd); err != nil {
+	if err := cmd.Run(); err != nil {
 		return ecerror.Wrap(err, cmd.ProcessState.ExitCode())
 	}
 	return nil
